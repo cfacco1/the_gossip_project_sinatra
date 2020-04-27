b88bf27c96c4498a56b1feef6f0eb087341020bd
cfacco1 <facco.clement@gmail.com>
2020-04-27 19:14:28 +0200

    comments

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -8,12 +8,14 @@ class Gossip
       @author = author
     end
 
+#Sauvegarder les potins dans un CSV
 	def save
   		CSV.open("./db/gossip.csv", "ab") do |csv|
     	csv << [@author, @content]
   		end
 	end
 
+#Enregistrer les potins dans un tableau
   def self.all
     all_gossips = []
     CSV.read("./db/gossip.csv").each do |csv_line|
@@ -22,6 +24,7 @@ class Gossip
     return all_gossips
   end
 
+#"Indexer"
   def self.find(id)
   gossips = []  
   CSV.read("./db/gossip.csv").each_with_index do |row, index|
